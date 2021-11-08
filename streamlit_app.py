ff3aa94af2c54b6c0ce84001b1f65278f4ac5cf1
Ateanna Uriri <26284980+aouriri@users.noreply.github.com>
2021-11-08 16:27:37 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,16 @@
 import streamlit as st
 
-st.sidebar.info('This is a purely informational message')
+app_formal_name = "Audio Conversion//Speech to Text//NER"
 
+st.sidebar.markdown("-----------------------------------")
+st.sidebar.markdown(
+    f"[{app_formal_name}](https://github.com/aouriri/pytranscriber) "
+    f"converts mp3 files to wav (with the option to download), transcribes that audio file to text using the [SpeechRecognition](https://pypi.org/project/SpeechRecognition/) Python library. "
+    f"The text can the be copied *OR* processed using [spaCy](https://github.com/explosion/spacy-streamlit). "
+)
+st.sidebar.markdown(
+    "Made with 💙 by [@aouriri (she/her)](https://github.com/aouriri) as a [LEADING](https://cci.drexel.edu/mrc/leading/) fellow."
+)
 st.write("mp3 to wav converter")
 
 st.write("Speech to Text converter")
