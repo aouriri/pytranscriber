8945b60b68ab9fc056aa812e0b825eb541c3a455
Ateanna Uriri <26284980+aouriri@users.noreply.github.com>
2021-11-08 15:01:28 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,5 @@
 import streamlit as st
-import wave, contextlib, math, time
-import speech_recognition as sr
-import ffmpeg
-from os import path
-from pydub import AudioSegment
-from moviepy.editor import AudioFileClip
+
 
 
 st.write(""" # My First App 
