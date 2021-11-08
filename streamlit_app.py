666ac7011e2a31068a1e9698b9a4427762406393
Ateanna Uriri <26284980+aouriri@users.noreply.github.com>
2021-11-08 16:31:50 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,11 +6,14 @@ st.sidebar.markdown("-----------------------------------")
 st.sidebar.markdown(
     f"[{app_formal_name}](https://github.com/aouriri/pytranscriber) "
     f"converts mp3 files to wav (with the option to download), transcribes that audio file to text using the [SpeechRecognition](https://pypi.org/project/SpeechRecognition/) Python library. "
-    f"The text can the be copied *OR* processed using [spaCy](https://github.com/explosion/spacy-streamlit). "
+    f"The text can the be copied **or** processed using [spaCy](https://github.com/explosion/spacy-streamlit). "
 )
+
 st.sidebar.markdown(
     "Made with 💙 by [@aouriri (she/her)](https://github.com/aouriri) as a [LEADING](https://cci.drexel.edu/mrc/leading/) fellow."
+    "Fellowship site: University of California - San Diego (UCSD) Library , Project name: [Transformation and Enhancement of the Farmworker Movement Collection](https://libraries.ucsd.edu/farmworkermovement/)"
 )
+
 st.write("mp3 to wav converter")
 
 st.write("Speech to Text converter")
