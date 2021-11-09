9c56bcb7be3ab41f68fcfa5a4982a495bf2cee7a
Ateanna Uriri <26284980+aouriri@users.noreply.github.com>
2021-11-09 15:30:22 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,13 +1,8 @@
+# based on Fanilo Andrianasolo's "Convert a MIDI file to WAV" Streamlit app
+
 import streamlit as st
 
 app_formal_name = "Audio Conversion//Speech to Text//NER"
-
-st.sidebar.markdown("-----------------------------------")
-st.sidebar.markdown(
-    f"[{app_formal_name}](https://github.com/aouriri/pytranscriber) "
-    f"converts mp3 files to wav (with the option to download), transcribes that audio file to text using the [SpeechRecognition](https://pypi.org/project/SpeechRecognition/) Python library. "
-    f"The text can the be copied to be used elsewhere **or** processed using [spaCy](https://github.com/explosion/spacy-streamlit). "
-)
 
 page = st.sidebar.selectbox('Select page',
   ['Audio Conversion','Speech to Text Transcription', 'Named Entity Recognition'])
@@ -20,6 +15,13 @@ else:
     # Display the transcription content here
     st.write("Speech to Text converter")
 
+st.sidebar.markdown("-----------------------------------")
+st.sidebar.markdown(
+    f"[{app_formal_name}](https://github.com/aouriri/pytranscriber) "
+    f"converts mp3 files to wav (with the option to download), transcribes that audio file to text using the [SpeechRecognition](https://pypi.org/project/SpeechRecognition/) Python library. "
+    f"The text can the be copied to be used elsewhere **or** processed using [spaCy](https://github.com/explosion/spacy-streamlit). "
+)
+
 st.sidebar.markdown(
     "Made with 💜 by [@aouriri (she/her)](https://github.com/aouriri) as a [LEADING](https://cci.drexel.edu/mrc/leading/) fellow."    
 )
@@ -29,6 +31,3 @@ st.sidebar.markdown(
         "Fellowship site: University of California - San Diego (UCSD) [Library](https://library.ucsd.edu/), Project name: [Transformation and Enhancement of the Farmworker Movement Collection](https://libraries.ucsd.edu/farmworkermovement/)"
 )
 
-# based on Fanilo Andrianasolo's "Convert a MIDI file to WAV" Streamlit app
-
-st.title('My First Streamlit App')
