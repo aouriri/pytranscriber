2a440161e9c95edba6169c2940cfd53c599b185b
Ateanna Uriri <26284980+aouriri@users.noreply.github.com>
2021-11-10 15:43:49 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 # based on Fanilo Andrianasolo's "Convert a MIDI file to WAV" Streamlit app
 
 import streamlit as st
+import spacy_streamlit
 
 app_formal_name = "Audio Conversion//Speech to Text//NER"
 
@@ -35,7 +36,20 @@ else:
     # Display the NER content here
     st.title("Named Entity Recognition")
     
-    st.run('https://raw.githubusercontent.com/aouriri/pytranscriber/main/spacyNER_code.py')
+    DEFAULT_TEXT = """Google was founded in September 1998 by Larry Page and Sergey Brin while they were Ph.D. students at Stanford University in California. Together they own about 14 percent of its shares and control 56 percent of the stockholder voting power through supervoting stock. They incorporated Google as a California privately held company on September 4, 1998, in California. Google was then reincorporated in Delaware on October 22, 2002."""
+    spacy_model = "en_core_web_sm"
+
+    st.title("My cool app")
+    text = st.text_area("Text to analyze", DEFAULT_TEXT, height=200)
+    doc = spacy_streamlit.process_text(spacy_model, text)
+    
+    spacy_streamlit.visualize_ner(
+    doc,
+    labels=["PERSON", "DATE", "GPE"],
+    show_table=False,
+    title="Persons, dates and locations",
+)
+st.text(f"Analyzed using spaCy model {spacy_model}")
 
 st.sidebar.markdown("-----------------------------------")
 st.sidebar.markdown(
