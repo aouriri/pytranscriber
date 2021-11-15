63183ce045cae743deedf6d5b12d65aaf463389c
Ateanna Uriri <26284980+aouriri@users.noreply.github.com>
2021-11-15 14:14:34 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,9 +38,9 @@ if page == 'Audio Conversion':
         r_page = sess.get(url, headers=user_agent)
         soup = BeautifulSoup(r_page.content, "html.parser")
         link = soup.find(lambda tag: tag.name == "a" and tag.has_attr("download"))
-	if link is None:
-        	st.error(f"No mp3 file found on page '{url}'")
-        	raise ValueError(f"No mp3 file found on page '{url}'")
+		if link is None:
+        		st.error(f"No mp3 file found on page '{url}'")
+        		raise ValueError(f"No mp3 file found on page '{url}'")
 
 	url_mp3_file = "http.*\.mp3"
 	r_mp3_file = sess.get(url_midi_file, headers=user_agent)
