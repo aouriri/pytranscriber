2fa12ac5463ce749f542b379c5e66adf151dfc95
Ateanna Uriri <26284980+aouriri@users.noreply.github.com>
2021-11-15 14:12:57 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,29 +29,29 @@ st.sidebar.markdown("-----------------------------------")
 page = st.sidebar.selectbox('Select page',
   ['Audio Conversion','Speech to Text Transcription', 'Named Entity Recognition'])
 if page == 'Audio Conversion':
-    # Display the conversion content here
-        st.title(":arrows_clockwise: mp3 to wav converter")
-        sess = load_session()
+# Display the conversion content here
+    st.title(":arrows_clockwise: mp3 to wav converter")
+    sess = load_session()
 	
-	def download_from_URL(url: str, sess: requests.Session) -> bytes:
-	   user_agent = {"User-agent": "bot"}
-	   r_page = sess.get(url, headers=user_agent)
-           soup = BeautifulSoup(r_page.content, "html.parser")
-           link = soup.find(lambda tag: tag.name == "a" and tag.has_attr("download"))
+    def download_from_URL(url: str, sess: requests.Session) -> bytes:
+        user_agent = {"User-agent": "bot"}
+        r_page = sess.get(url, headers=user_agent)
+        soup = BeautifulSoup(r_page.content, "html.parser")
+        link = soup.find(lambda tag: tag.name == "a" and tag.has_attr("download"))
 	if link is None:
-	   st.error(f"No mp3 file found on page '{url}'")
-	   raise ValueError(f"No mp3 file found on page '{url}'")
+        st.error(f"No mp3 file found on page '{url}'")
+        raise ValueError(f"No mp3 file found on page '{url}'")
 
 	url_mp3_file = "http.*\.mp3"
 	r_mp3_file = sess.get(url_midi_file, headers=user_agent)
 	return r_mp3_file.content
         
-        uploaded_mp3 = st.file_uploader("Upload mp3 file", type=["mp3"])
-        mp3_link = st.text_input(
-            "Or input URL", "https://libraries.ucsd.edu/farmworkermovement/media/oral_history/music/Huelga%203%20Cesar%20Chavez.mp3"
-        )
+    uploaded_mp3 = st.file_uploader("Upload mp3 file", type=["mp3"])
+    mp3_link = st.text_input(
+        "Or input URL", "https://libraries.ucsd.edu/farmworkermovement/media/oral_history/music/Huelga%203%20Cesar%20Chavez.mp3"
+    )
         
-        run_button = st.button('Convert!')
+    run_button = st.button('Convert!')
 
 elif page == 'Speech to Text Transcription':
     # Display the transcription content here
@@ -72,4 +72,3 @@ st.sidebar.markdown(
 st.sidebar.markdown(
     "**Fellowship site:** University of California - San Diego (UCSD) [Library](https://library.ucsd.edu/), **Project name:** [Transformation and Enhancement of the Farmworker Movement Collection](https://libraries.ucsd.edu/farmworkermovement/)"
 )
-
