eac84d7f99d7ee4297fd7d8ce862225cbc2bc21e
Ateanna Uriri <26284980+aouriri@users.noreply.github.com>
2021-11-15 09:45:40 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,12 @@
 # based on Fanilo Andrianasolo's "Convert a MIDI file to WAV" Streamlit app
 
+import io
+import numpy as np
+import requests
 import streamlit as st
 import spacy
 import spacy_streamlit
+from bs4 import BeautifulSoup
 
 app_formal_name = "Audio Conversion//Speech to Text//NER"
 
@@ -29,7 +33,69 @@ if page == 'Audio Conversion':
     # Display the conversion content here
     st.title("mp3 to wav converter")
     
-    mp3 = st.file_uploader("Upload mp3 file.", type=["mp3"])
+    # mp3 = st.file_uploader("Upload mp3 file.", type=["mp3"])
+        
+    @st.cache(allow_output_mutation=True)
+    def load_session():
+	return requests.Session()
+
+    def has_download_attr(tag):
+	return tag.has_attr("download")
+
+    @st.cache(
+	hash_funcs={requests.Session: id},
+	allow_output_mutation=True,
+	suppress_st_warning=True,
+    )
+def download_from_site(url: str, sess: requests.Session) -> bytes:
+    user_agent = {"User-agent": "bot"}
+    r_page = sess.get(url, headers=user_agent)
+    soup = BeautifulSoup(r_page.content, "html.parser")
+    link = soup.find(lambda tag: tag.name == "a" and tag.has_attr("download"))
+    if link is None:
+        st.error(f"No mp3 file found on page '{url}'")
+        raise ValueError(f"No mp3 file found on page '{url}'")
+
+    url_mp3_file = "http.*\.mp3"
+    r_mp3_file = sess.get(url_mp3_file, headers=user_agent)
+    return r_mp3_file.content
+
+
+def main():
+    st.title(":arrows_clockwise: mp3 to wav converter")
+    sess = load_session()
+
+    uploaded_mp3 = st.file_uploader("Upload mp3 file.", type=["mp3"])
+    mp3_link = st.text_input(
+        "Or input URL", "https://libraries.ucsd.edu/farmworkermovement/media/oral_history/music/Huelga%203%20Cesar%20Chavez.mp3"
+    )
+
+    mp3_file = None
+
+    if uploaded_file is None:
+        if "http.*\.mp3" not in mp3_link:
+            st.error("Make sure your URL is of type 'http.*\.mp3'")
+            st.stop()
+        with st.spinner(f"Downloading mp3 file from {mp3_link}"):
+            mp3_file = io.BytesIO(download_from_bitmidi(mp3_link, sess))
+    else:
+        mp3_file = uploaded_mp3
+
+    st.markdown("---")
+
+   # with st.spinner(f"Transcribing to FluidSynth"):
+   #     midi_data = pretty_midi.PrettyMIDI(midi_file)
+   #     audio_data = midi_data.fluidsynth()
+   #     audio_data = np.int16(
+   #         audio_data / np.max(np.abs(audio_data)) * 32767 * 0.9
+   #     )  # -- Normalize for 16 bit audio https://github.com/jkanner/streamlit-audio/blob/main/helper.py
+
+   #     virtualfile = io.BytesIO()
+   #     wavfile.write(virtualfile, 44100, audio_data)
+
+   # st.audio(virtualfile)
+   # st.markdown("Download the audio by selecting the "⋮" and selecting "Download" or by right-clicking on the media player")
+        
 elif page == 'Speech to Text Transcription':
     # Display the transcription content here
     st.title("Speech to Text Transcription")
