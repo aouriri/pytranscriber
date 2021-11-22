1bb3aa5deb193e2040c138b89988a537107e359e
Ateanna Uriri <26284980+aouriri@users.noreply.github.com>
2021-11-22 15:27:28 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,11 +47,11 @@ if page == 'Audio Conversion':
 	
 	def convert_mp3_to_wav(uploaded_file):
 		sound = AudioSegment.from_mp3(uploaded_file)
-		export = sound.export("converted_file.wav", format="wav")
+		sound.export("converted_file.wav", format="wav")
 		
 	st.download_button(
 		label="Download wav file",
-		data=export,
+		data=uploaded_file,
 		mime=None,
 	)
         
