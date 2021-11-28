b2956aaa690909b06f3c7536c14730f84affc67e
Ateanna Uriri <26284980+aouriri@users.noreply.github.com>
2021-11-28 16:26:39 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -96,7 +96,6 @@ elif page == 'Speech to Text Transcription':
 	#	text = r.recognize_google(audio_data)
     
     	#transcribedtxt = st.text_area("Transcribed text", text)
-
 	st.markdown("Speech to text using ```Python``` can be done 'out of the box' on short audio (less than/equal to a minute) using Google's Speech Recognition." 
 		"The code for that is included below. For larger audio files, an API must be used."
 		"For this project, I am using [IBM's Speech to Text](https://www.ibm.com/cloud/watson-speech-to-text) and its cloud." 
