00a122aad470da20114acefad0ed53549b8b522c
Ateanna Uriri <26284980+aouriri@users.noreply.github.com>
2021-11-29 17:15:40 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,8 +3,7 @@ import numpy as np
 import requests
 import time
 import speech_recognition as sr
-import urllib3
-import urllib3.request as url
+import urllib3 as url
 import streamlit as st
 import spacy
 import spacy_streamlit
