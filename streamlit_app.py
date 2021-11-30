fa2f2072ff24f051bb44d7fcc32aae998aebe889
Ateanna Uriri <26284980+aouriri@users.noreply.github.com>
2021-11-30 13:43:16 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -54,7 +54,7 @@ if page == 'Audio Conversion':
 	
 	#if uploaded_file is None:
 	#	with st.spinner(f"Downloading mp3 file from {mp3_link}"):
-			mp3_file = mp3_link
+	#		mp3_file = mp3_link
 	#else:
 	#	mp3_file = uploaded_file
 	
