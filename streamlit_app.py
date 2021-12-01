2890cfc0a75b65e010bfd48114fe86274d2e9dce
Ateanna Uriri <26284980+aouriri@users.noreply.github.com>
2021-12-01 16:21:42 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -103,20 +103,19 @@ else:
 	# Remove components out of spacy/streamlit wrapper to test spacy opentapioca
 	st.title('Named Entity Recognition')
 		   
-	import spacy
+	DEFAULT_TEXT = """Google was founded in September 1998 by Larry Page and Sergey Brin while they were Ph.D. students at Stanford University in California. Together they own about 14 percent of its shares and control 56 percent of the stockholder voting power through supervoting stock. They incorporated Google as a California privately held company on September 4, 1998, in California. Google was then reincorporated in Delaware on October 22, 2002."""
 	
-	nlp = spacy.blank("en")
+	spacy_model = "en_core_web_sm"
 	nlp.add_pipe('opentapioca')
-	doc = nlp("Google was founded in September 1998 by Larry Page and Sergey Brin while they were Ph.D. students at Stanford University in California. Together they own about 14 percent of its shares and control 56 percent of the stockholder voting power through supervoting stock. They incorporated Google as a California privately held company on September 4, 1998, in California. Google was then reincorporated in Delaware on October 22, 2002.")
-	params = {"text": doc.text,
-		  "ents": [{"start": ent.start_char,
-			    "end": ent.end_char,
-			    "label": ent.label_,
-			    "kb_id": ent.kb_id_,
-			    "kb_url": "https://www.wikidata.org/entity/" + ent.kb_id_}
-			   for ent in doc.ents],
-		  "title": None}
-	spacy.displacy.serve(params, style="ent", manual=True)
+	text = st.text_area("Text to analyze (Default text can be used, but I'm okay with change.)", DEFAULT_TEXT, height=200)
+	doc = spacy_streamlit.process_text(spacy_model, text)
+	
+	spacy_streamlit.visualize_ner(
+		doc,
+		labels=["PERSON", "DATE", "GPE", "ORG", "NORP", "LAW", "LOC"],
+		show_table=False,
+		title="Person, Places and Other Things"
+	)
 
 	with st.expander("Entity label explanation"):
 		st.write("""
@@ -135,4 +134,4 @@ else:
 			**DATE:**        Absolute or relative dates or periods.
 	 """)
 	
-	st.text(f'Analyzed using spaCy model {nlp}.')
+	st.text(f'Analyzed using spaCy model {spacy_model}.')
