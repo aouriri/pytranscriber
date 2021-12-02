b738c8cdda95fa984382e302be8b0edd12de78b8
Ateanna Uriri <26284980+aouriri@users.noreply.github.com>
2021-12-02 17:22:37 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -123,16 +123,18 @@ else:
 		   
 	DEFAULT_TEXT = """Google was founded in September 1998 by Larry Page and Sergey Brin while they were Ph.D. students at Stanford University in California. Together they own about 14 percent of its shares and control 56 percent of the stockholder voting power through supervoting stock. They incorporated Google as a California privately held company on September 4, 1998, in California. Google was then reincorporated in Delaware on October 22, 2002."""
 	
-	spacy_model = "en_core_web_sm"
+	nlp = spacy.load("en_core_web_sm")
+	nlp.add_pipe('opentapioca')
 	text = st.text_area("Text to analyze (Default text can be used, but I'm okay with change.)", DEFAULT_TEXT, height=200)
-	doc = spacy_streamlit.process_text(spacy_model, text)
+	doc = nlp(text)
+	visualize_ner(doc, labels=nlp.get_pipe("ner").labels)
 	
-	spacy_streamlit.visualize_ner(
-		doc,
-		labels=["PERSON", "DATE", "GPE", "ORG", "NORP", "LAW", "LOC"],
-		show_table=False,
-		title="Person, Places and Other Things"
-	)
+	#spacy_streamlit.visualize_ner(
+	#	doc,
+	#	labels=["PERSON", "DATE", "GPE", "ORG", "NORP", "LAW", "LOC"],
+	#	show_table=False,
+	#	title="Person, Places and Other Things"
+	#)
 
 	with st.expander("Entity label explanation"):
 		st.write("""
