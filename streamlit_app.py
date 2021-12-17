b04da7179930a876d6597a5f78d8ad1124889fcb
Ateanna Uriri <26284980+aouriri@users.noreply.github.com>
2021-12-16 19:41:23 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -122,7 +122,7 @@ else:
 	DEFAULT_TEXT = """Google was founded in September 1998 by Larry Page and Sergey Brin while they were Ph.D. students at Stanford University in California. Together they own about 14 percent of its shares and control 56 percent of the stockholder voting power through supervoting stock. They incorporated Google as a California privately held company on September 4, 1998, in California. Google was then reincorporated in Delaware on October 22, 2002."""
 	
 	text = st.text_area("Text to analyze (Default text can be used, but I'm okay with change.)", DEFAULT_TEXT, height=200)
-	doc = (spacy_model, text)
+	doc = spacy_streamlit.process_text(spacy_model, text)
 	
 	spacy_streamlit.visualize_ner(
 		doc,
