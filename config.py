569690581b1d9ad11a09a9b77e73b46745e76b90
Ateanna Uriri <26284980+aouriri@users.noreply.github.com>
2021-12-20 15:14:32 -0600

    Update config.py

--- config.py
+++ config.py
@@ -1,2 +1,2 @@
-apikey = 'y7fac84LHBP2dM59V3bOB5gMfPWE6Bda3FWQoI2mNtdD'
+apikey = 'ukV8EJBdUXjJAlwgCCzqla3tytWHGPFOgjTJtJK-4ea5'
 url = 'https://api.us-south.speech-to-text.watson.cloud.ibm.com/instances/c00c4369-2081-4e93-bb07-19eb25f72125'
