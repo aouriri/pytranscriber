650920a59f231330bd43e943eecc649d0215ce4f
Ateanna Uriri <26284980+aouriri@users.noreply.github.com>
2021-12-29 17:38:36 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -151,6 +151,13 @@ else:
 
 	text = st.text_area("Text to analyze (Default text can be used, but I'm okay with change.)", DEFAULT_TEXT, height=200)
 	doc = spacy_streamlit.process_text("en_core_web_sm", text)
+	
+	label_select = exp.multiselect(
+		"Entity labels",
+		options=labels,
+		default=list(labels),
+		key=f"{key}_ner_label_select",
+        )
 
 	spacy_streamlit.visualize_ner(
 		doc,
