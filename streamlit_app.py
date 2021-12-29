e7bff5a390790520d1fe26444b6dea5d3c858368
Ateanna Uriri <26284980+aouriri@users.noreply.github.com>
2021-12-29 16:59:18 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -155,7 +155,8 @@ else:
 	spacy_streamlit.visualize_ner(
 		doc,
 		labels=["PERSON", "DATE", "GPE", "ORG", "NORP", "LAW", "LOC"],
-		kb_url_template=f"https://www.wikidata.org/wiki/{ent_kb_id}",
+		kb_id=ent_kb_id
+		kb_url_template=f"https://www.wikidata.org/wiki/{kb_id}",
 		show_table=False,
 		title="Person, Places and Other Things",
 	)
