64907a5f394bef41a82a2b1ffe4be5d46a7ddba8
Ateanna Uriri <26284980+aouriri@users.noreply.github.com>
2021-12-29 18:32:29 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,6 @@ import streamlit as st
 import spacy
 import spacy_streamlit
 import en_core_web_sm
-from spacy import displacy
 from bs4 import BeautifulSoup
 from os import path
 from pydub import AudioSegment
@@ -153,27 +152,12 @@ else:
 	text = st.text_area("Text to analyze (Default text can be used, but I'm okay with change.)", DEFAULT_TEXT, height=200)
 	doc = spacy_streamlit.process_text("en_core_web_sm", text)
 
-	html = displacy.render(
+	spacy_streamlit.visualize_ner(
 		doc,
-		style="ent",
-		colors={},
-		options=[{"start": ent.start_char,
-			  "end": ent.end_char,
-			  "label": ent.label_,
-			  "kb_id": ent.kb_id_,
-			  "kb_url": "https://www.wikidata.org/entity/" + ent.kb_id_} 
-			 for ent in doc.ents],
-		manual=True,
-		#show_table=False,
-		#title="Person, Places and Other Things",
-        )
-	
-	#spacy_streamlit.visualize_ner(
-	#	doc,
-	#	labels=["PERSON", "DATE", "GPE", "ORG", "NORP", "LAW", "LOC"],
-	#	show_table=False,
-	#	title="Person, Places and Other Things",
-	#)
+		labels=["PERSON", "DATE", "GPE", "ORG", "NORP", "LAW", "LOC"],
+		show_table=False,
+		title="Person, Places and Other Things",
+	)
 
 	with st.expander("Entity label explanation"):
 		st.write("""
