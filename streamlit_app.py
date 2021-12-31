d0c669281050ac058f4b473691698a1e32adc612
Ateanna Uriri <26284980+aouriri@users.noreply.github.com>
2021-12-31 16:09:18 -0600

    updates to STT.

--- streamlit_app.py
+++ streamlit_app.py
@@ -91,6 +91,8 @@ if page == 'Audio Conversion':
 elif page == 'Speech to Text Transcription':
 # Display the transcription content here
 	import config
+	from config import apikey
+	from config import url
 	authenticator = IAMAuthenticator(config.apikey)
 	stt = SpeechToTextV1(authenticator=authenticator)
 	stt.set_service_url(config.url)
@@ -105,25 +107,27 @@ elif page == 'Speech to Text Transcription':
 		   )
 
 	fileObject = st.file_uploader("Please upload your file", type=["wav"])
+	if uploaded_file is not None:
+		     bytes_data = fileObject.getvalue()
 
-	# with open(join(dirname('__file__'), r'C:/Users/atsr0/Documents/WFH/LEADINGfellowship/HuelgaCesarChavez.wav'),'rb') as audio_file:
-	# 	dic = json.loads(
-	# 		json.dumps(
-	# 			service.recognize(
-	# 				audio=audio_file,
-	# 				content_type='audio/wav',
-	# 				timestamps=False,
-	# 				word_confidence=False).get_result(),
-	# 			indent=2))
-	#
-	# # Stores the transcribed text
-	# str = ""
-	#
-	# while bool(dic.get('results')):
-	# 	str = dic.get('results').pop().get('alternatives').pop().get('transcript')+str[:]
+	with open(join(dirname('__file__'), bytes_data),'rb') as audio_file:
+	 	dic = json.loads(
+	 		json.dumps(
+	 			service.recognize(
+	 				audio=audio_file,
+	 				content_type='audio/wav',
+	 				timestamps=False,
+	 				word_confidence=False).get_result(),
+	 			indent=2))
+
+	# Stores the transcribed text
+	 str = ""
+
+	 while bool(dic.get('results')):
+	 	str = dic.get('results').pop().get('alternatives').pop().get('transcript')+str[:]
 
 	st.markdown("---")
-	st.text_area('Transcribed text', ' ') # replace quotes with str
+	st.text_area('Transcribed text', str) # replace quotes with str
 	st.markdown("---")
 
 	with st.expander("Speech Recognition (Basic) Code"):
@@ -144,7 +148,7 @@ elif page == 'Speech to Text Transcription':
 else:
 	# Display the NER content here
 	# Example using the components provided by spacy-streamlit in an existing app.
-	
+
 	st.title('Named Entity Recognition')
 
 	DEFAULT_TEXT = """Google was founded in September 1998 by Larry Page and Sergey Brin while they were Ph.D. students at Stanford University in California. Together they own about 14 percent of its shares and control 56 percent of the stockholder voting power through supervoting stock. They incorporated Google as a California privately held company on September 4, 1998, in California. Google was then reincorporated in Delaware on October 22, 2002."""
