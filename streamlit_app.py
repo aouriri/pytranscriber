4156fba651f6173fade6c867de0c429e8a6dbdd5
Ateanna Uriri <26284980+aouriri@users.noreply.github.com>
2022-01-03 16:16:54 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -124,6 +124,7 @@ elif page == 'Speech to Text Transcription':
 	 				audio=audio_file,
 	 				content_type='audio/wav',
 	 				timestamps=False,
+					model='en-US_NarrowbandModel',
 	 				word_confidence=False).get_result(),
 	 			indent=2))
 
