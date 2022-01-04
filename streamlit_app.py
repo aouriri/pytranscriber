53fc1f6e5d130b3a3d7ccf1cfcfe58d455dc477e
Ateanna Uriri <26284980+aouriri@users.noreply.github.com>
2022-01-04 11:38:32 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -139,7 +139,7 @@ elif page == 'Speech to Text Transcription':
 	trns_content = st.text_area('Transcribed text', str, height=150, placeholder="Future location of transcribed text.")
 	st.download_button(
 		label="Download transcribed text",
-		data=trns_content,
+		data=str,
 		mime='text/plain',
 	)
 	st.markdown("Text downloaded as .txt file.")
