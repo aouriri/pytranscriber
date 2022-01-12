e17b4c12e762b986d3762bd0162d42ddd7c21757
Ateanna Uriri <26284980+aouriri@users.noreply.github.com>
2022-01-12 15:05:18 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,7 +33,7 @@ st.set_page_config(
 		f"The text can the be copied to be used elsewhere **or** processed using [spaCy](https://github.com/explosion/spacy-streamlit). "
 		}
 )
-st.sidebar.markdown(["pytranscriber logo"](pytrnscrbr_logo.png))
+st.sidebar.image(pytrnscrbr_logo.png, caption="py transcriber")
 st.sidebar.markdown("-----------------------------------")
 
 page = st.sidebar.selectbox('Select page',
