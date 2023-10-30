03fbc5a0e3c7dceb93dafe56434c7aff963fe1db
Ateanna Uriri <26284980+aouriri@users.noreply.github.com>
2023-10-30 14:50:38 -0500

    Update streamlit_app.py
    fixing NER portion.

--- streamlit_app.py
+++ streamlit_app.py
@@ -205,9 +205,13 @@ else:
 
 	DEFAULT_TEXT = """Google was founded in September 1998 by Larry Page and Sergey Brin while they were Ph.D. students at Stanford University in California. Together they own about 14 percent of its shares and control 56 percent of the stockholder voting power through supervoting stock. They incorporated Google as a California privately held company on September 4, 1998, in California. Google was then reincorporated in Delaware on October 22, 2002."""
 
-	nlp = en_core_web_sm.load()
+	spacy_model = "en_core_web_sm"
+	nlp = spacy.load(spacy_model)
+	nlp.add_pipe("opentapioca")
+	nlp.to_disk("/tmp/en_core_web_sm_ot")
+	
 	text = st.text_area("Text to analyze (Default text can be used, but I'm okay with change.)", DEFAULT_TEXT, height=200)
-	doc = spacy_streamlit.process_text("en_core_web_sm", text)
+	doc = spacy_streamlit.process_text("/tmp/en_core_web_sm_ot", text)
 
 	spacy_streamlit.visualize_ner(
 		doc,
